e82caf1cf2d853f4c954bfea9e4e963eb7c2ef43
Mango <JennMeng@Jenns-MacBook-Pro.local>
2016-10-27 10:31:29 -0700

    second commit

--- app.js
+++ app.js
@@ -5,23 +5,30 @@ $(document).ready(function(){
 	new WOW().init();
 
 	$(window).on("scroll", function() {
-	    var scrollDistance = $(window).scrollTop();
-	    if(scrollDistance >= 50) {
-	    	$("#navbar-nav").addClass("affix");
+	    var scrollDistance= $(window).scrollTop();
+	    
+	    // if(scrollDistance > 50) {
+	    // 	$(".nav navbar-nav").addClass("affix");
+	    // } else {
+	    // 	$(".nav navbar-nav").removeClass("affix");
+	    // }
+	    if(scrollDistance > 50) {
+	    	$(".navbar navbar-default navbar-fixed-top").removeClass("affix-top").addClass("affix");
 	    } else {
-	    	$("navbar-nav").removeClass("affix");
+	    	$(".navbar navbar-default navbar-fixed-top").removeClass("affix").addClass("affix-top");
 	    }
 	});
 
-	$(document).on("click", ".page-scroll", function(){
+	$(document).on("click", ".page-scroll", function(event){
+		event.preventDefault();
 		var target= $(this).attr("href");
 
 		$("body").animate({
     		scrollTop: $(target).offset().top
-    	}, 1000);	
+    	},2000);	
 	});
 
-	$("body").scrollspy({target: "#havbar-nav"});
+	$("body").scrollspy({target: ".navbar navbar-default navbar-fixed-top"});
 
 
 });
